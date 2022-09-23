cce86445970e1ef11b69212e58f423773733d06a
Brian Williams <heffergod@gmail.com>
2022-09-23 10:07:58 -0700

    unit tests complete

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,18 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('This should test to see if the item returned is an array', () => {
+        const arrTest = typeof shuffleArray([1,2,3,4,5])
+        expect(arrTest).toBe("object")
+    })
+
+    test('This should test that all the same items are in the array', () => {
+        const arr = [0,1,2,3,4]
+        let tester = true
+
+        if (arr !== shuffleArray(arr)){
+            tester = false
+        }
+        expect(tester).toBeFalsy()
+    })
 })
